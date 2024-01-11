20ebbaa2b3a28a16b4ee22c1de1d6b95e147aa43
Ernesto Bautista <bautistacuellar.5@gmail.com>
2024-01-11 13:39:31 -0600

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import "./navBar.css"
+
+export const NavBar = () => {
+  return (
+    <div>
+        <h1 class="text-primary">
+            ¡Bienvenidos a Laminas en Puebla .Com!
+        </h1>
+    </div>
+  )
+}
